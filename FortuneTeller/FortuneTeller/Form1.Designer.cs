cfb13827000c72819a038ea8ff6b33a622a758e9
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-07-29 21:51:42 +0300

    FortuneTeller

---
+++ FortuneTeller/FortuneTeller/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FortuneTeller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button);
+            this.splitContainer1.Size = new System.Drawing.Size(288, 152);
+            this.splitContainer1.SplitterDistance = 74;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 70);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(0, 0);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(284, 70);
+            this.button.TabIndex = 0;
+            this.button.Text = "PREDICT\r\n";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 152);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ FortuneTeller/FortuneTeller/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace FortuneTeller
+{
+    public partial class Form1 : Form
+    {
+        private const string appName = "Fortune Teller";
+        private readonly string preditionsConfig = $"{Environment.CurrentDirectory}\\tsconfig1.json";
+        private string[] predictions;
+        private Random random = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, EventArgs e)
+        {
+            button.Enabled = false;
+            await Task.Run(() =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        if(i == progressBar1.Maximum)
+                        {
+                            progressBar1.Maximum = i + 1;
+                            progressBar1.Value = i + 1;
+                            progressBar1.Maximum = i;
+                        }
+                        else
+                        {
+                            progressBar1.Value = i + 1;
+                        }
+                        progressBar1.Value = i;
+                        this.Text = $"{i}%";
+                    }));
+                    Thread.Sleep(50);
+                }
+            });
+
+            var index = random.Next(predictions.Length);
+            var prediction = predictions[index];
+            MessageBox.Show($"{prediction}");
+            progressBar1.Value = 0;
+            this.Text = appName;
+            button.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = appName;
+            try
+            {
+                var data = File.ReadAllText(preditionsConfig);
+                predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(predictions == null)
+                {
+                    Close();
+                }
+                else if(predictions.Length == 0)
+                {
+                    MessageBox.Show("Предсказания закончились");
+                    Close();
+                }
+            }
+        }
+    }
+}
